b60c341bf1518c2b1a2590dd000009e7a593f156
Nate <n_phan@btiworld.com>
2016-09-25 17:46:35 -0400

    Added yahoo stock api and currency converter jar. Also using njit db

--- src/main/java/com/cs490/database/MySqlConnector.java
+++ src/main/java/com/cs490/database/MySqlConnector.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.jasypt.util.text.BasicTextEncryptor;
 
 public class MySqlConnector {
@@ -40,6 +41,40 @@ public class MySqlConnector {
 			return null;
 		}
 		return conn;
+	} 
+	
+	public BasicDataSource  getDataSource() throws Exception {
+		BasicDataSource dataSource = null;
+		try {
+			InputStream in = getClass().getClassLoader().getResourceAsStream("mysql.properties");
+			Properties props = new Properties();
+			if (in != null) {
+				props.load(in);
+				String user = props.getProperty("user");
+				String encryptedPassword = props.getProperty("password");
+				String host = props.getProperty("host");
+				String driver = props.getProperty("driver");
+				Class.forName(driver).newInstance();
+				BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+				textEncryptor.setPassword("keyForPass"); 
+				String decryptedPassword = textEncryptor.decrypt(encryptedPassword);
+				dataSource = new BasicDataSource();
+				dataSource.setDriverClassName(driver);
+				dataSource.setUsername(user);
+				dataSource.setPassword(decryptedPassword);
+				dataSource.setUrl(host);
+				dataSource.setMaxIdle(5);
+				dataSource.setInitialSize(5);
+				dataSource.setValidationQuery("SELECT 1");
+			} else {
+				System.err.println("Uh oh properties file not found. Abort mission!");
+				return null;
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+		return dataSource;
 	} 
 
 }
